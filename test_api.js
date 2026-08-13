d44b7c935321ce3a54a3229c0ce8e7fc5a59b151
Djoystick <j.j.niccals2@gmail.com>
2026-08-13 08:07:17 +0300

    chore: clean up legacy import scripts and json dumps

--- test_api.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const token = process.env.TELEGRAM_BOT_TOKEN;
-const fileId = 'BQACAgIAAyEGAATsaaHLAAIBRWp8_anGjubrJlNpdoJvlvfzSt-KAAJ9oQACwqDpSzX0wQABmi4pIj0E';
-fetch('https://api.telegram.org/bot' + token + '/getFile?file_id=' + fileId)
-  .then(res => res.json())
-  .then(data => console.log(data));
